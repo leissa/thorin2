70fd219697dbb77a4d33636f4a9f4ddd62fa4d9d
Roland Leißa <leissa@uni-mannheim.de>
2024-03-06 20:02:46 +0100

    loop example

--- src/thorin/plug/memoir/normalizers.cpp
+++ src/thorin/plug/memoir/normalizers.cpp
@@ -29,14 +29,22 @@ Ref normalize_write(Ref type, Ref callee, Ref arg) {
 
     if (auto assoc = match<memoir::assoc>(c)) {
         if (auto tuple = assoc->arg()->isa<Tuple>()) {
-            auto KV = assoc->decurry()->arg();
-            auto n  = assoc->decurry()->decurry()->arg();
+            auto n  = assoc->decurry()->arg();
+            auto KV = assoc->decurry()->decurry()->arg();
             if (auto l = Lit::isa(n)) {
                 DefVec new_ops;
-                for (size_t i = 0, e = *l; i != e; ++i)
-                    new_ops.emplace_back(tuple->proj(e, i));
-                new_ops.emplace_back(world.tuple({k, v}));
-                return world.call<memoir::assoc>(new_ops.size(), KV, Defs(new_ops));
+                bool updated = false;
+                for (size_t i = 0, e = *l; i != e; ++i) {
+                    auto kv = tuple->proj(e, i);
+                    if (kv->proj(2, 0) == k) {
+                        updated = true;
+                        new_ops.emplace_back(world.tuple({k, v}));
+                    } else {
+                        new_ops.emplace_back(tuple->proj(e, i));
+                    }
+                }
+                if (!updated) new_ops.emplace_back(world.tuple({k, v}));
+                return world.call<memoir::assoc>(KV, new_ops.size(), Defs(new_ops));
             }
         }
     }
@@ -46,16 +54,26 @@ Ref normalize_write(Ref type, Ref callee, Ref arg) {
 
 Ref normalize_has(Ref type, Ref callee, Ref arg) {
     auto& world = type->world();
+    auto [c, k] = arg->projs<2>();
+    if (auto assoc = match<memoir::assoc>(c)) {
+        if (auto tuple = assoc->arg()->isa<Tuple>()) {
+            for (auto kv : tuple->ops()) {
+                if (kv->proj(2, 0) == k) return world.lit_tt();
+            }
+        }
+    }
     return world.raw_app(type, callee, arg);
 }
 
 Ref normalize_size(Ref type, Ref callee, Ref arg) {
     auto& world = type->world();
+    if (auto assoc = match<memoir::assoc>(arg)) return assoc->decurry()->arg();
     return world.raw_app(type, callee, arg);
 }
 
 Ref normalize_keys(Ref type, Ref callee, Ref arg) {
     auto& world = type->world();
+    // TODO
     return world.raw_app(type, callee, arg);
 }
 
